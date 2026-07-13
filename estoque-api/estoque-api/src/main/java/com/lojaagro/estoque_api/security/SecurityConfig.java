74ebcb70db0c8d9a17bb7375b3c069591b47e07a
Ivano Gabriel <gabrimelo90@gmail.com>
2026-07-13 11:55:00 -0300

    subindo ping

--- estoque-api/estoque-api/src/main/java/com/lojaagro/estoque_api/security/SecurityConfig.java
+++ estoque-api/estoque-api/src/main/java/com/lojaagro/estoque_api/security/SecurityConfig.java
@@ -17,26 +17,25 @@ public class SecurityConfig {
     public SecurityConfig(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
     }
-
+    
     @Bean
 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .cors(cors -> cors.configurationSource(request -> {
-        var config = new org.springframework.web.cors.CorsConfiguration();
-        config.setAllowedOrigins(java.util.List.of("*"));
-        config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE"));
-        config.setAllowedHeaders(java.util.List.of("*"));
-        return config;
-    }))
-    .csrf(csrf -> csrf.disable())
-        .csrf(csrf -> csrf.disable())
+            var config = new org.springframework.web.cors.CorsConfiguration();
+            config.setAllowedOrigins(java.util.List.of("*"));
+            config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE"));
+            config.setAllowedHeaders(java.util.List.of("*"));
+            return config;
+        }))
+        .csrf(csrf -> csrf.disable()) 
         .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/auth/registrar", "/auth/login").permitAll()
-            .anyRequest().authenticated()
+            .requestMatchers(org.springframework.http.HttpMethod.GET, "/ping").permitAll() 
         )
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
+        
     return http.build();
 }
 
