16695ef047ae988eb48b779fbe695f46eb8d0906
Ivano Gabriel <gabrimelo90@gmail.com>
2026-07-13 11:47:04 -0300

    adicionando uma rota publica pro CRONOJOB ficar pingando

--- estoque-api/estoque-api/src/main/java/com/lojaagro/estoque_api/controllers/ProdutoController.java
+++ estoque-api/estoque-api/src/main/java/com/lojaagro/estoque_api/controllers/ProdutoController.java
@@ -17,7 +17,10 @@ public class ProdutoController {
     public ProdutoController(ProdutoService service) {
         this.service = service;
     }
-
+    @GetMapping("/ping")
+public ResponseEntity<String> ping() {
+    return ResponseEntity.ok("ok");
+}
     @GetMapping
     public ResponseEntity<List<Produto>> buscarTodos() {
         return ResponseEntity.ok(service.buscarTodos());
