7bc22e7f8f52dd0f06d342eed7cebf6d5419cc76
Ivano Gabriel <gabrimelo90@gmail.com>
2026-09-02 14:42:18 -0300

    consertando, ajustes finos e mudança de layout total.

--- estoque-api/estoque-api/src/main/java/com/lojaagro/estoque_api/security/SecurityConfig.java
+++ estoque-api/estoque-api/src/main/java/com/lojaagro/estoque_api/security/SecurityConfig.java
@@ -2,37 +2,27 @@ package com.lojaagro.estoque_api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
-    private final JwtFilter jwtFilter;
-
-    public SecurityConfig(JwtFilter jwtFilter) {
-        this.jwtFilter = jwtFilter;
-    }
+    // variável jwtFilter e o construtor removidos - lembrannnndo que no futuro tlvz tenha login
 
     @Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(csrf -> csrf.disable())
-        .cors(org.springframework.security.config.Customizer.withDefaults()) // LEMBRANDO AQUI PRA ALTERAR DEEPOIS SE FOR PREECISO( QUEEM VEER ISSO, SAIBA Q TIREI A TELA DEE LOGIN INICIAL SO PRA FACILITAR A PRODUÇÃO)
-        .authorizeHttpRequests(auth -> auth
-            .anyRequest().permitAll() // Libera TUDO sem precisar de login(TEMPORARIAMENTE, LEMBRAR!!)
-        );
-    
-    return http.build();
-}
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors(org.springframework.security.config.Customizer.withDefaults()) // LEMBRANDO AQUI PRA ALTERAR DEPOIS SE FOR PRECISO
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll() // Libera TUDO sem precisar de login(TEMPORARIAMENTE, LEMBRAR!!)
+            );
+        
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
